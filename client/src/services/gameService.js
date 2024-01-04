a5f1fe3bd8ca351e5b690c7563ef426394e78ee4
AtanasDimitrovGIT <At.Dimitrov.99@gmail.com>
2024-01-04 14:31:55 +0200

    Add get all games service method
    Add abstract request

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,7 +1,18 @@
-const baseUrl = 'http://localhost:5174/jsonstore'
+
+import {request} from "../lib/request";
+
+const baseUrl = 'http://localhost:3030/jsonstore/games'
+
+export const getAll = async() => {
+   const result = await request ('GET', baseUrl);
+
+   console.log(result);
+
+   return Object.values(result);
+};
 
 export const create = async(gameData) => {
-    const response = await fetch(`${baseUrl}/games`, {
+    const response = await fetch(baseUrl, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
